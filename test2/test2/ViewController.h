32152c9d3d38697c0abc9492df6ac8341212efdc
wei <wei@qq.com>
2015-08-30 22:21:13 -0700

    xxx

---
+++ test2/test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by admin on 15/8/30.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
